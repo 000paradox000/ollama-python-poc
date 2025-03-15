3f38aa998065dd509a5afdfba4d7c3473d924bc7
('Ariel <ariel.calzada@gmail.com>
2025-03-15 03:25:31 -0500

    Polish the llm handler

--- libs/llm_handler.py
+++ libs/llm_handler.py
@@ -3,20 +3,28 @@ from ollama import ChatResponse
 
 
 class LLMHandler:
-    MODEL_NAME = "gemma3:1b"
-    DUMMY_QUESTION = "is the creator of dragon ball z alive"
+    """
+    A handler for interacting with the Ollama chat model.
 
-    def __init__(self):
-        pass
+    Attributes:
+        MODEL_NAME (str): The name of the model to be used.
+        DUMMY_QUESTION (str): A predefined question to test the model.
+    """
+
+    MODEL_NAME: str = "gemma3:1b"
+    DUMMY_QUESTION: str = "is the creator of dragon ball z alive"
 
     def make_a_dummy_question(self) -> str:
+        """
+        Sends a predefined question to the chat model and returns the response.
+
+        Returns:
+            str: The content of the model's response.
+        """
         response: ChatResponse = chat(
             model=self.MODEL_NAME,
             messages=[
-                {
-                    "role": "user",
-                    "content": self.DUMMY_QUESTION,
-                },
+                {"role": "user", "content": self.DUMMY_QUESTION},
             ],
         )
         return response["message"]["content"]
